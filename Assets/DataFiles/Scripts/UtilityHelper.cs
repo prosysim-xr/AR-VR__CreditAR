8ba1205f0d91f1a4103b2067b18cf45479cef0e1
Suman Singh <suman@i2ce.in>
2020-04-16 14:38:00 -0700

    added the full project

---
+++ Assets/DataFiles/Scripts/UtilityHelper.cs
@@ -0,0 +1,45 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class UtilityHelper : MonoBehaviour
+{
+    
+    //
+    public static WebCamTexture backCam;
+
+    private void Start()
+    {
+       
+    }
+
+    public static void InitializeCamera()
+    {
+        //WebCamDevice[] devices = WebCamTexture.devices;
+        //backCam = new WebCamTexture(devices[0].name);
+        //backCam = new WebCamTexture(devices[0].name, Screen.width, Screen.height);
+        backCam = new WebCamTexture();
+        //print(backCam.GetPixel());
+        print(backCam.GetPixels());
+        
+    }
+    public static void PlayCamera()
+    { 
+        //playCam_GameObject.GetComponent<Renderer>().material.mainTexture = backCam;
+
+        if (!backCam.isPlaying)
+            backCam.Play();
+    }
+
+    //public static void CaptureCamera(WebCamTexture back_cam)
+    //{
+        
+    //}
+
+    public static void StopCamera()
+    {
+        backCam.Stop();
+    }
+
+}
